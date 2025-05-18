d6b293e513f70e9b884ffa78cb29ddd4a2f1e883
VforVog <stelios_vogiatzis@hotmail.com>
2025-05-18 20:56:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
+import {API} from "aws-amplify";
 import {
   Button,
   Flex,
@@ -9,15 +10,12 @@ import {
   TextField,
   View,
   withAuthenticator,
-  Image,
 } from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-import { API, Storage } from "aws-amplify";
-
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
@@ -29,15 +27,6 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
     setNotes(notesFromAPI);
   }
 
@@ -45,13 +34,10 @@ const App = ({ signOut }) => {
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
-    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
-      image: image.name,
     };
-    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -61,10 +47,9 @@ const App = ({ signOut }) => {
   }
 
 
-  async function deleteNote({ id, name }) {
+  async function deleteNote({ id }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-    await Storage.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -132,7 +117,7 @@ const App = ({ signOut }) => {
           </Flex>
         ))}
       </View>
-      
+
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
